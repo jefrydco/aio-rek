8b2c00bc11a9aafcfb8d269d9a951f669cdc9bc4
Jefry Dewangga <jefrydco@gmail.com>
2020-04-26 22:24:31 +0700

    test: update syntax to match vuetify 2 and vee validate 3

--- test/unit/specs/layouts/default.spec.js
+++ test/unit/specs/layouts/default.spec.js
@@ -1,20 +1,27 @@
 import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
 import Vuetify from 'vuetify'
-import VeeValidate from 'vee-validate'
 
 import Default from '~/layouts/default'
 
 const localVue = createLocalVue()
 localVue.use(Vuetify)
-localVue.use(VeeValidate)
 
 describe('Default layout', () => {
+  let vuetify
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
   it('renders to match snapshot', () => {
     const wrapper = mount(Default, {
       localVue,
+      vuetify,
       stubs: {
         nuxt: 'nuxt',
-        'router-link': RouterLinkStub
+        'router-link': RouterLinkStub,
+        'nuxt-link': RouterLinkStub,
+        'nuxt-child': '<div></div>',
+        'validation-provider': '<div></div>',
+        'validation-observer': '<div></div>'
       }
     })
     expect(wrapper).toMatchSnapshot()
