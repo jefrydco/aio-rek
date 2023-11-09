5b3d3e0ed594ed95933780643947f490067fd0ab
JiteraDeveloper <jitera-developer@jitera.com>
2023-11-09 11:13:24 +0000

    feat: Implement business logic 'User authentication'

--- api/app/users/controller.js
+++ api/app/users/controller.js
@@ -1,22 +1,27 @@
 'use strict'
-
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const errorCatcher = require('async-error-catcher').default
 const Controller = require('../base/Controller')
-
 class UserController extends Controller {
   constructor() {
     super(UserController.name)
   }
-
   login(req, res, next) {
-    return errorCatcher((req, res) => {
-      const { user } = req
+    return errorCatcher(async (req, res) => {
+      const { username, password } = req.body
       const service = this._getService(res)
-      return res.json({ [this.name]: service.getAuthJSON(user) })
+      const result = await service.authenticateUser(username, password)
+      if (result.token) {
+        // Log successful login attempt
+        await service.logLoginAttempt(username, 'successful')
+        return res.json({ token: result.token })
+      } else {
+        // Log failed login attempt
+        await service.logLoginAttempt(username, 'failed')
+        return res.status(400).json({ error: result.error })
+      }
     })(req, res, next)
   }
-
   fetchAuth(req, res, next) {
     return errorCatcher((req, res) => {
       const { user } = req
@@ -25,7 +30,6 @@ class UserController extends Controller {
       return res.json({ [this.name]: service.getAuthJSON(user, token) })
     })(req, res, next)
   }
-
   fetchProfile(req, res, next) {
     return errorCatcher((req, res) => {
       const { user } = req
@@ -34,5 +38,4 @@ class UserController extends Controller {
     })(req, res, next)
   }
 }
-
 module.exports = new UserController()
--- api/app/users/service.js
+++ api/app/users/service.js
@@ -1,15 +1,14 @@
 'use strict'
-
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const config = require('../../config')
-
+const User = require('../models/User')
+const LoginAttempt = require('../models/LoginAttempt')
 const Service = require('../base/Service')
-
 class UserService extends Service {
   constructor(app) {
     super(UserService.name, app, ['hashed_password'])
   }
-
   generateJWT(user) {
     const role = user.get('role')
     return jwt.sign(
@@ -21,13 +20,25 @@ class UserService extends Service {
       { algorithm: 'RS256', expiresIn: role === 'device' ? '10y' : '1h' }
     )
   }
-
   getAuthJSON(user, token) {
     return {
       ...this.toJSON(user, ['id']),
       token: token || this.generateJWT(user)
     }
   }
+  async authenticateUser(username, password) {
+    const user = await User.findOne({ where: { username } })
+    if (!user) {
+      throw new Error('User not found')
+    }
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      await LoginAttempt.create({ user_id: user.id, timestamp: new Date(), status: 'failed' })
+      throw new Error('Invalid password')
+    }
+    const token = this.generateJWT(user)
+    await LoginAttempt.create({ user_id: user.id, timestamp: new Date(), status: 'successful' })
+    return this.getAuthJSON(user, token)
+  }
 }
-
 module.exports = (app) => new UserService(app)
