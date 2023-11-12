e758d8d1b04f8ab0f9a25d5374fdc422f65c6eeb
Margarethe (Jitera) <developers@jitera.com>
2023-11-12 18:38:29 +0000

    feat[business-logic]: implement 'Password update' Changes: * '/api/app/users/router.js': : + Add a new route for the password update function. + The route should be a POST route to '/users/update-password'. + Use bodyParser.json() to parse the incoming request body. + Use createTransaction to start a new transaction. + Use jwtAuth.required to ensure that the user is authenticated. + Use...

--- api/app/users/router.js
+++ api/app/users/router.js
@@ -1,3 +1,4 @@
+// PATH: /api/app/users/router.js
 const bodyParser = require('body-parser')
 const router = require('express').Router()
 const {
@@ -17,15 +18,89 @@ const {
   destroy,
   forgotPassword,
   resetPassword,
-  checkKYCStatus
+  updatePassword // import the updatePassword function
 } = require('./controller')
-const { resetPasswordRules } = require('./validation');
-// Existing routes...
+const { resetPasswordRules, updatePasswordRules } = require('./validation'); // import the updatePasswordRules function
+router.post(
+  '/users/login',
+  bodyParser.json(),
+  createTransaction,
+  localAuth,
+  login
+)
+router.post(
+  '/users',
+  bodyParser.json(),
+  createTransaction,
+  jwtAuth.required,
+  handleRole('admin'),
+  create
+)
 router.get(
-  '/api/users/:id/kyc',
+  '/users',
+  createTransaction,
+  jwtAuth.required,
+  handleRole('admin'),
+  fetchPage
+)
+router.get(
+  '/users/:id',
+  createTransaction,
+  jwtAuth.required,
+  handleRole('admin'),
+  fetch
+)
+router.put(
+  '/users/:id',
+  bodyParser.json(),
+  createTransaction,
+  jwtAuth.required,
+  handleRole('admin'),
+  update
+)
+router.delete(
+  '/users/:id',
+  createTransaction,
+  jwtAuth.required,
+  handleRole('admin'),
+  destroy
+)
+router.get(
+  '/user/auth',
+  createTransaction,
+  jwtAuth.required,
+  handleRole([['admin'], ['device']]),
+  fetchAuth
+)
+router.get(
+  '/user/profile',
+  createTransaction,
+  jwtAuth.required,
+  handleRole([['admin'], ['device']]),
+  fetchProfile
+)
+router.post(
+  '/users/forgot-password',
+  bodyParser.json(),
+  createTransaction,
+  forgotPassword
+)
+router.post(
+  '/users/reset-password',
+  bodyParser.json(),
   createTransaction,
   jwtAuth.required,
   handleRole('user'),
-  checkKYCStatus
+  resetPasswordRules,
+  resetPassword
+)
+router.post(
+  '/users/update-password',
+  bodyParser.json(),
+  createTransaction,
+  jwtAuth.required,
+  handleRole('user'),
+  updatePasswordRules,
+  updatePassword
 );
 module.exports = router
